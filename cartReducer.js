e96d32db47c63529d1c4939389958eab64821dde
Chris McClarin <eternalrook@gmail.com>
2023-11-07 02:04:40 -0800

    cartReducer complete

--- cartReducer.js
+++ cartReducer.js
@@ -19,15 +19,52 @@ const products = [
     { id: 3, name: "Ultra Ball", price: 30 }
 ];
 
+// once the reducer is ready, we can look at the calculate tool. We can use a simple reduce function for this.
 function calculateTotal(cart) {
-    return 0;
+    return cart.reduce((total, product) => {
+        return total + product.quantity * product.price;
+    }, 0);
 }
 
 const initialState = [];
 
+// First things first: Add the differnt action types and the error to the reducer
 function reducer(cart, action) {
-    return cart;
-}
+// I added some code to find the item in the cart, since both the add and the decrement need that info.
+    const inCart = cart.find((item) => item.id === action.id)
+
+    if (action.type === "add") {
+        if (!inCart) {
+        // in the add action type, we need to add a new item with a quantity key,
+            return [...cart, { ...products[action.id-1], quantity: 1 }]; // the products[action.id-1] isn't the best practice, since id's aren't usually numbered like that.
+        }
+        // or modify the existing items quantity
+        return cart.map((item) =>
+            item.id === action.id ? { ...item, quantity: item.quantity + 1 } : item
+            );
+    }
+    if (action.type === "update") {
+    // in the update action type, we need to know the action adjustment.
+        if (action.adjustment === "increment") {
+            //Increment is the same as add action, so we just repeat the code of the add function.
+            return cart.map((item) =>
+                item.id === action.id ? { ...item, quantity: item.quantity + 1 } : item
+            );
+        }
+    // the decrement(not under an if statment since it is the only other option) needs to verify if the quantity is 1,
+        if (item.quantity === 1) {
+            // a quantity of 0 should remove the product from the cart. 
+            return cart.filter((item) => item.id !== action.id);
+        } else {
+            // otherwise it should just subtract 1 from the quantity
+            return cart.map((item) =>
+                item.id === action.id ? { ...item, quantity: item.quantity - 1 } : item
+            );
+        }
+    }
+    throw new Error("This action type isn't supported.")
+
+    }
 
 export default function ShoppingCart() {
     const [cart, dispatch] = React.useReducer(reducer, initialState);
