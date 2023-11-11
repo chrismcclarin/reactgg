845ee3590aecddce2d4de4bf23a09ff6edd6ae22
Chris McClarin <eternalrook@gmail.com>
2023-11-10 22:20:59 -0800

    reactRulerAdvEff complete

--- reactRulerAdvEff.js
+++ reactRulerAdvEff.js
@@ -7,13 +7,30 @@
 import * as React from "react";
 
 export default function ReactRuler() {
+    // ResizeObserver API requires a DOM node to observe, therefore we add a useRef hook to "grab" a node.
+    const ref = React.useRef(null)
     const [width, setWidth] = React.useState(null);
+
+    // now we use useLayoutEffect() to create a new instance of ResizeObserver and observe our reference.
+    // useLayoutEffect() does the same thing as useEffect(), except that useLayoutEffect() happens before the ui paints the screen.
+    React.useLayoutEffect(()=>{
+        const observer = new ResizeObserver(([entry]) => {
+            setWidth(entry.borderBoxSize[0].inlineSize)
+        })
+        //our resize obvserver will observe our reference
+        observer.observe(ref.current)
+
+        return ()=>{
+            //upon re-render, observer will disconnect, then reconnect when the page finishes re-rendering.
+            observer.disconnect()
+        }
+    }, [])
 
     return (
         <section>
             <h1>React Ruler</h1>
             <p>(Resize the ruler)</p>
-            <article>
+            <article ref={ref}>
                 <label>width: {Math.floor(width)}</label>
             </article>
         </section>
